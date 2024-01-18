f6967bf4aadde511b41cf5ca1a5656b8ebcddc65
EvgeniBondarev <bond.evg03@mail.ru>
2024-01-18 21:27:32 +0300

    clear branch

--- main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from routing.bus import router as buses_router
-
-app = FastAPI()
-
-app.include_router(buses_router)
--- shemas/park.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class ParkShema(BaseModel):
-    name: str
