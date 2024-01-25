cbc2fed233a6c626e431ed4ce0d14219100a280d
EvgeniBondarev <bond.evg03@mail.ru>
2024-01-25 14:39:31 +0300

    change timezon

--- utils/date_manager.py
+++ utils/date_manager.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 def generate_dates():
     current_datetime = datetime.now()
 
-    first_date = current_datetime.now()
+    first_date = current_datetime.now() + timedelta(hours=3)
     second_date = first_date + timedelta(minutes=30)
 
     formatted_first_date = first_date.strftime('%d.%m.%Y %H:%M')
